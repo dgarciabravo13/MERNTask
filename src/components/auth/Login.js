a1e8a68c00abe74affee09de7841b4bbc76f8fb5
David Garcia <dgarciabravo13@gmail.com>
2020-08-28 20:57:56 +0200

    working in the Login component

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,46 @@
 import React from 'react';
 
+const onChange = () =>{
+
+}
+
 const Login = () => {
-  return ( <h1>Desde Login</h1> );
+  return ( 
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Log in</h1>
+        <form>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="tu Email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="tu Password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-block btn-primario"
+              value="Iniciar Sesion"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+    );
 }
  
 export default Login;
