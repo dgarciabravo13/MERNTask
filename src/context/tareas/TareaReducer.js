40215af3d4629ccc23f92471bc9741f2ae482f0e
David Garcia <dgarciabravo13@gmail.com>
2020-10-02 14:39:22 +0200

    add dispatch for new tasks

--- src/context/tareas/TareaReducer.js
+++ src/context/tareas/TareaReducer.js
@@ -1,5 +1,6 @@
 import {
-  TAREAS_PROYECTO
+  TAREAS_PROYECTO,
+  AGREGAR_TAREA
 } from "../../types";
 
 
@@ -11,6 +12,11 @@ export default (state, action) => {
         ...state, 
         tareasproyecto: state.tareas.filter(tarea => tarea.proyectoId === action.payload)
       }
+      case AGREGAR_TAREA:
+        return {
+          ...state,
+          tareas: [...state.tareas, action.payload]
+        }
     default: 
       return state;
   }
--- src/context/tareas/TareaState.js
+++ src/context/tareas/TareaState.js
@@ -4,7 +4,8 @@ import TareaContext from "./TareaContext";
 import TareaReducer from "./TareaReducer";
 
 import {
-  TAREAS_PROYECTO
+  TAREAS_PROYECTO,
+  AGREGAR_TAREA
 } from "../../types";
 
  const TareaState = props => {
@@ -42,12 +43,22 @@ import {
      })
    }
 
+   //Agregar una tarea al proyecto seleccionado
+
+   const agregarTarea = tarea => {
+     dispatch({
+       type:AGREGAR_TAREA,
+       payload: tarea
+     })
+   }
+
    return (
      <TareaContext.Provider
       value={{
         tareas: state.tareas,
         tareasproyecto: state.tareasproyecto,
-        obtenerTareas
+        obtenerTareas,
+        agregarTarea
       }}
      >
        {props.children}
