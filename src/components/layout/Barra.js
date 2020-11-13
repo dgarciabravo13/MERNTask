fbdb61e27b45bf6ca7e40131cfe00d089546e331
David Garcia <dgarciabravo13@gmail.com>
2020-11-13 14:35:42 +0100

    showing the user name

--- src/components/layout/Barra.js
+++ src/components/layout/Barra.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React,{useContext, useEffect} from 'react';
+import AuthContext from "../../context/autenticacion/authContext";
 
 const Barra = () => {
+
+    //Extraer la información de autenticacion 
+    const authContext = useContext(AuthContext);
+    const {usuario, usuarioAutenticado} = authContext;
+  
+    useEffect(()=>{
+      usuarioAutenticado()
+      // eslint-disable-next-line
+    },[])
   return ( 
     <header className="app-header">
-      <p className="nombre-usuario">Hola <span>David</span></p>
+      {usuario ?<p className="nombre-usuario">Hola <span>{usuario.nombre}</span></p> : null}
       <nav className="nav-principal">
         <a href="#!">Cerrar Sesión</a>
       </nav>
